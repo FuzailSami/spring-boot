6e822c72a0170be471773a76705c862452d98d19
Fuzail Sami <samifuzail@gmail.com>
2025-06-30 15:54:43 -0500

    commit1

---
+++ src/main/java/com/fuzail/SoftwareEngineerController.java
@@ -0,0 +1,27 @@
+package com.fuzail;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                      1,
+                      "James",
+                        "js,node,react,tailwindcss"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Jamila",
+                        "jave,spring,springboot"
+                )
+        );
+    }
+}
