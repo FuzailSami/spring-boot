28d3a3ab19467df9f18398fd87ad301fb00012c6
Fuzail Sami <samifuzail@gmail.com>
2025-07-01 13:21:34 -0500

    commit2

--- src/main/java/com/fuzail/SoftwareEngineerController.java
+++ src/main/java/com/fuzail/SoftwareEngineerController.java
@@ -1,27 +1,29 @@
 package com.fuzail;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+    private final SoftwareEngineerService softwareEngineerService;
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
     @GetMapping
     public List<SoftwareEngineer> getEngineers() {
-        return List.of(
-                new SoftwareEngineer(
-                      1,
-                      "James",
-                        "js,node,react,tailwindcss"
-                ),
-                new SoftwareEngineer(
-                        2,
-                        "Jamila",
-                        "jave,spring,springboot"
-                )
-        );
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(
+            @PathVariable Integer id
+    ) {
+        return softwareEngineerService.getAllSoftwareEngineerById(id);
+
+    }
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 }
